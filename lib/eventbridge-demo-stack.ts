9581c9edf08cf36afcd024b40ecd2f433f40218a
entest <hai@entest.io>
2022-08-21 15:07:41 +0000

    first

--- lib/eventbridge-demo-stack.ts
+++ lib/eventbridge-demo-stack.ts
@@ -1,16 +1,100 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  Stack,
+  StackProps,
+  aws_s3,
+  RemovalPolicy,
+  aws_lambda,
+  aws_events,
+  aws_events_targets,
+  aws_s3_notifications,
+  aws_dynamodb,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as path from "path";
+import * as fs from "fs";
 
-export class EventbridgeDemoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class EventBridgeStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // table
+    const table = new aws_dynamodb.Table(this, "EventMessageTable", {
+      tableName: "EventMessageTable",
+      partitionKey: {
+        name: "id",
+        type: aws_dynamodb.AttributeType.STRING,
+      },
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EventbridgeDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // new s3 bucket
+    const bucket = new aws_s3.Bucket(this, "bucket-eventbrideg-demo", {
+      bucketName: `eventbridge-demo-${this.account}`,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    bucket.enableEventBridgeNotification();
+
+    // lambda function
+    const func = new aws_lambda.Function(this, "ProcessS3Event", {
+      functionName: "ProcessS3Event",
+      code: aws_lambda.Code.fromInline(
+        fs.readFileSync(path.resolve(__dirname, "./../lambda/index.py"), {
+          encoding: "utf-8",
+        })
+      ),
+      handler: "index.handler",
+      runtime: aws_lambda.Runtime.PYTHON_3_7,
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    table.grantReadWriteData(func);
+    bucket.grantReadWrite(func);
+
+    // first method - event bridge rule
+    const rule = new aws_events.Rule(this, "S3EventTriggerLambdaRule", {
+      ruleName: "S3EventTriggerLambdaRule",
+      eventPattern: {
+        source: ["aws.s3"],
+        detailType: ["Object Created"],
+        detail: {
+          bucket: {
+            name: [bucket.bucketName],
+          },
+          object: {
+            key: [{ prefix: "onrule/" }],
+          },
+        },
+      },
+    });
+
+    rule.addTarget(new aws_events_targets.LambdaFunction(func));
+
+    // second method - onEvent
+    bucket.addEventNotification(
+      aws_s3.EventType.OBJECT_CREATED,
+      new aws_s3_notifications.LambdaDestination(func),
+      { prefix: "onevent" }
+    );
+
+    // test lambda put objects into s3
+    const testLambda = new aws_lambda.Function(this, "testLambda", {
+      functionName: "testLambda",
+      code: aws_lambda.Code.fromInline(
+        fs.readFileSync(
+          path.resolve(__dirname, "./../lambda/lambda-put-item-s3.py"),
+          { encoding: "utf-8" }
+        )
+      ),
+      handler: "index.handler",
+      runtime: aws_lambda.Runtime.PYTHON_3_8,
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      },
+    });
+
+    bucket.grantReadWrite(testLambda);
   }
 }
